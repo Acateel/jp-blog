eeaf9eafce35c739c023660d9e629c7d2d807ba4
Taras Kozodoi <acateely@gmail.com>
2023-01-25 15:10:04 +0200

    add show users in posts

--- src/component/posts/Post.jsx
+++ src/component/posts/Post.jsx
@@ -4,8 +4,14 @@ import "./Post.css";
 const Post = (props) => {
   return (
     <div className="post">
-      <h1 className="post_title">{props.post.title}</h1>
-      <p className="post_body">{props.post.body}</p>
+      <div className="user">
+        <div className="user_link"></div>
+        <p className="user_name">{props.user.username}</p>
+      </div>
+      <div className="post_content">
+        <h1 className="post_title">{props.post.title}</h1>
+        <p className="post_body">{props.post.body}</p>
+      </div>
     </div>
   );
 };
--- src/component/posts/PostsList.jsx
+++ src/component/posts/PostsList.jsx
@@ -1,15 +1,16 @@
 import { useEffect } from "react";
 import { connect } from "react-redux";
-import { fetchPosts } from "../../actions";
+import { fetchPosts, fetchUsers } from "../../actions";
 import Post from "./Post";
 
 const PostsList = (props) => {
   useEffect(() => {
     props.fetchPosts();
+    props.fetchUsers();
   }, []);
 
   const renderedPostsList = () =>
-    props.posts.map((post) => <Post key={post.id} post={post} />);
+    props.posts.map((post) => <Post key={post.id} post={post} user={props.users[post.userId]} />);
 
   if (!props.posts) {
     return <h3>Loading...</h3>;
@@ -26,7 +27,8 @@ const PostsList = (props) => {
 const mapStateToProps = (state) => {
   return {
     posts: state.posts,
+    users: state.users,
   };
 };
 
-export default connect(mapStateToProps, { fetchPosts })(PostsList);
+export default connect(mapStateToProps, { fetchPosts, fetchUsers })(PostsList);
