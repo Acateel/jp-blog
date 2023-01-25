cfeb96b43a99367c87f01e2e7921768e49ef88f6
Taras Kozodoi <acateely@gmail.com>
2023-01-25 12:40:49 +0200

    add fetch user

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -1,7 +1,12 @@
-import { FETCH_POSTS } from "./types";
+import { FETCH_POSTS, FETCH_USER } from "./types";
 import JSONPlaceholder from "../apis/JSONPlaceholder";
 
 export const fetchPosts = () => async (dispatch) => {
-  const response = await JSONPlaceholder("/posts");
+  const response = await JSONPlaceholder.get("/posts");
   dispatch({ type: FETCH_POSTS, payload: response.data });
+};
+
+export const fetchUser = (id) => async (dispatch, getState) => {
+  const response = await JSONPlaceholder.get(`/users/${id}`);
+  dispatch({ type: FETCH_USER, payload: response.data });
 };
