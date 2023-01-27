4336cb3709f7fd28bb225c97a7f1eef45bfb5e6d
Taras Kozodoi <acateely@gmail.com>
2023-01-27 13:05:14 +0200

    add comnets

--- src/component/posts/PostShow.jsx
+++ src/component/posts/PostShow.jsx
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
-import { fetchPost, fetchUser } from "../../actions";
+import { fetchPost, fetchUser, fetchPostComments } from "../../actions";
 import Post from "./Post";
+import "./Comment.css"
 
 const PostShow = (props) => {
   const { id } = useParams();
@@ -10,6 +11,7 @@ const PostShow = (props) => {
   const post = props.posts[id];
   if (!post) {
     props.fetchPost(id);
+    props.fetchPostComments(id)
     return <h1 className="title">Loading...</h1>;
   }
 
@@ -19,14 +21,29 @@ const PostShow = (props) => {
     return <h1 className="title">Loading...</h1>;
   }
 
-  return <Post post={post} user={props.users[post.userId]} />;
+  const renderedComments = () => props.comments?.map((comment)=>(
+    <div className="comment" key={comment.id}>
+      <p className="comment_email">{comment.email}</p>
+      <p className="comment_name">{comment.name}</p>
+      <p className="comment_body">{comment.body}</p>
+    </div>
+  ))
+
+  return (
+    <Fragment>
+      <Post post={post} user={props.users[post.userId]} />
+      <h1 className="title">Comments</h1>
+      {renderedComments()}
+    </Fragment>
+  );
 };
 
 const mapStateToProps = (state) => {
   return {
     posts: state.posts,
     users: state.users,
+    comments: state.comments,
   };
 };
 
-export default connect(mapStateToProps, { fetchPost, fetchUser })(PostShow);
+export default connect(mapStateToProps, { fetchPost, fetchUser, fetchPostComments })(PostShow);
