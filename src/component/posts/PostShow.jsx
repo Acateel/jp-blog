68aa10a00813044e0b22791e12e1f91b3038c204
Taras Kozodoi <acateely@gmail.com>
2023-01-27 12:45:31 +0200

    add post in postshow compnent

--- src/component/posts/PostShow.jsx
+++ src/component/posts/PostShow.jsx
@@ -1,5 +1,32 @@
-const PostShow = () => {
-  return <h1>Post Show</h1>;
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchPost, fetchUser } from "../../actions";
+import Post from "./Post";
+
+const PostShow = (props) => {
+  const { id } = useParams();
+
+  const post = props.posts[id];
+  if (!post) {
+    props.fetchPost(id);
+    return <h1 className="title">Loading...</h1>;
+  }
+
+  const user = props.users[post.userId];
+  if (!user) {
+    props.fetchUser(post.userId);
+    return <h1 className="title">Loading...</h1>;
+  }
+
+  return <Post post={post} user={props.users[post.userId]} />;
 };
 
-export default PostShow;
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts,
+    users: state.users,
+  };
+};
+
+export default connect(mapStateToProps, { fetchPost, fetchUser })(PostShow);
