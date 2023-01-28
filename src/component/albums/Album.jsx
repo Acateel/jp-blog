c561680c64c649000678ff170b8afa07911f0a44
Taras Kozodoi <acateely@gmail.com>
2023-01-28 15:09:20 +0200

    add photos in album component

--- src/component/albums/Album.jsx
+++ src/component/albums/Album.jsx
@@ -1,5 +1,63 @@
-const Album = () => {
-  return <h1 className="title">album</h1>;
+import { Fragment, useEffect } from "react";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
+import UserIcon from "../users/UserIcon";
+import {
+  fetchAlbumPhotos,
+  removeAlbumPhotos,
+  fetchAlbum,
+  fetchUser,
+} from "../../actions";
+import "./Album.css";
+
+const Album = (props) => {
+  const { id } = useParams();
+
+  useEffect(() => {
+    props.fetchAlbum(id);
+    props.fetchAlbumPhotos(id);
+    return () => {
+      props.removeAlbumPhotos();
+    };
+  }, []);
+
+  const album = props.albums[id];
+  const user = props.users[album.userId];
+  if (!user && album) {
+    props.fetchUser(album.userId);
+  }
+
+  if (!user || !album || !props.photos) {
+    return <h1 className="title">Loading...</h1>;
+  }
+
+  const renderedPhotos = () =>
+    props.photos.map((photo) => (
+      <img src={photo.url} alt={photo.title} key={photo.id} />
+    ));
+
+  return (
+    <Fragment>
+      <div className="album_title">
+        <h1 className="title">Album: {album.title}</h1>
+        <UserIcon className="user_link" name={user.username} id={user.id} />
+      </div>
+      <div className="photos_list">{renderedPhotos()}</div>
+    </Fragment>
+  );
 };
 
-export default Album;
+const mapStateToProps = (state) => {
+  return {
+    albums: state.albums,
+    photos: state.photos,
+    users: state.users,
+  };
+};
+
+export default connect(mapStateToProps, {
+  fetchAlbumPhotos,
+  removeAlbumPhotos,
+  fetchAlbum,
+  fetchUser,
+})(Album);
