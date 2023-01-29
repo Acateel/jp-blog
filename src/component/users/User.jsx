5865206f421a6882a8d7906aa881dbbf29518b05
Taras Kozodoi <acateely@gmail.com>
2023-01-29 18:28:45 +0200

    change tags in user component

--- src/component/users/User.jsx
+++ src/component/users/User.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
 import { connect } from "react-redux";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { fetchUser } from "../../actions";
 import UserIcon from "./UserIcon";
-import "./User.css"
+import "./User.css";
 
 const User = (props) => {
   const { id } = useParams();
@@ -16,28 +16,36 @@ const User = (props) => {
     return <h1 className="title">Loading...</h1>;
   }
 
-  const user = props.users[id]
+  const user = props.users[id];
   return (
     <div className="user_info">
-      <UserIcon className="user_info_icon user_icon" name={user.username ?? ""} id={user.id} />
+      <UserIcon
+        className="user_info_icon user_icon"
+        name={user.username ?? ""}
+        id={user.id}
+        size="80px"
+      />
       <div className="user_info_data">
         <section>
-            Name: <span>{user.name}</span>
+          Name: <span>{user.name}</span>
         </section>
         <section>
-            Username: <span>{user.username}</span>
+          Username: <span>{user.username}</span>
         </section>
         <section>
-            Email: <span>{user.email}</span>
+          Email: <a href={`mailto:${user.email}`}>{user.email}</a>
         </section>
         <section>
-            City: <span>{user.address.city}</span>
+          Address:
+          <address>{user.address.city}</address>
+          <address>{user.address.street}</address>
+          <address>{user.address.suite}</address>
         </section>
         <section>
-            Phone: <span>{user.phone}</span>
+          Phone: <a href={`tel:${user.phone}`}>{user.phone}</a>
         </section>
         <section>
-            Website: <a href={user.website} >{user.website}</a>
+          Website: <a href={user.website}>{user.website}</a>
         </section>
       </div>
     </div>
